a7b49ac6a8b242079cfc0bd58304706d5f270e68
equal-l2 <eng.equall2@gmail.com>
2024-10-27 19:42:16 +0900

    fixed json path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createSignal } from "solid-js";
-import conversionTable from "../../table_generator/unicode_table.json";
+import conversionTable from "./assets/unicode_table.json";
 
 const maxBase = conversionTable.length;
 
